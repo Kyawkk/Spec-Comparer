279561f947dd0241662b45d7f24c2924129d762e
KyawZinLinnEtag <157773274+KyawZinLinnEtag@users.noreply.github.com>
2024-02-26 16:50:34 +0630

    Fixed same height of home screen items

--- feature/home/src/main/java/com/kyawzinlinn/speccomparer/home/HomeScreen.kt
+++ feature/home/src/main/java/com/kyawzinlinn/speccomparer/home/HomeScreen.kt
@@ -5,11 +5,15 @@ package com.kyawzinlinn.speccomparer.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -26,9 +30,15 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,10 +54,14 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     onNavigateSearch: (ProductType, String) -> Unit,
 ) {
+    val density = LocalDensity.current
+    var desiredItemMinHeight by remember {
+        mutableStateOf(0.dp)
+    }
+
     Box(
         modifier = modifier.fillMaxSize(),
     ) {
-
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(16.dp),
@@ -60,7 +74,16 @@ fun HomeScreen(
                     displayCard = it,
                     onCardClick = { type, title ->
                         onNavigateSearch(type, title)
-                    }
+                    },
+                    modifier = Modifier
+                        .onPlaced {
+                            with(density) {
+                                if (desiredItemMinHeight < it.size.height.toDp()) {
+                                    desiredItemMinHeight = it.size.height.toDp()
+                                }
+                            }
+                        }
+                        .defaultMinSize(minHeight = desiredItemMinHeight)
                 )
             }
         }
@@ -71,11 +94,19 @@ fun HomeScreen(
 private fun ProductItemCard(
     displayCard: DisplayCard,
     onCardClick: (ProductType, String) -> Unit,
-    modifier: Modifier = Modifier.height(IntrinsicSize.Max)
+    modifier: Modifier = Modifier
 ) {
+    val density = LocalDensity.current
+    var desiredItemMinHeight by remember {
+        mutableStateOf(0.dp)
+    }
+
     val context = LocalContext.current
     val title = context.resources.getString(displayCard.title)
-    Card(modifier = modifier.fillMaxHeight(), onClick = { onCardClick(displayCard.type,title) }) {
+    Card(
+        modifier = modifier,
+        onClick = { onCardClick(displayCard.type,title) }
+    ) {
         Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.Start) {
             Icon(painter = painterResource(displayCard.icon), contentDescription = null)
             Spacer(Modifier.height(8.dp))
